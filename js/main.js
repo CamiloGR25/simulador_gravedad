1e2799bbb8421d00bcf93b08cbb90cdd4a284f59
Camilo <camilogomezr25@gmail.com>
2023-08-31 12:59:16 -0500

    Agregar planetas faltantes

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ altura;
 gravedad;
 velocidad;
 tiempo;
+pixelMetros;
 function simulador(){
     a=setInterval(function(){
         document.querySelector("#objeto").style.marginTop=contador+"px";
         contador+=velocidad;
-        if(contador>500){
+        console.log(gravedad);
+        if(contador>pixelMetros){
             clearInterval(a);
             document.querySelector(".modal-body").style.marginBottom="0px";
             bandera=true
@@ -16,22 +18,70 @@ function simulador(){
         }
     },500/(tiempo*1000))
 }
+function guardarAltura(){
+    altura=$("#altura").val();
+    document.getElementById("altura").value="";
+}
+function ajustarDatosMercurio(){
+    gravedad=3.7;
+    document.getElementById("titulo").innerText+="la tierra";
+    ajustarDatos();
+}
+function ajustarDatosVenus(){
+    gravedad=8.87;
+    document.getElementById("titulo").innerText+="la tierra";
+    ajustarDatos();
+}
+function ajustarDatosTierra(){
+    gravedad=9.807;
+    document.getElementById("titulo").innerText+="la tierra";
+    ajustarDatos();
+}
+function ajustarDatosMarte(){
+    gravedad=3.71;
+    document.getElementById("titulo").innerText+="Marte";
+  
+    ajustarDatos();
+}
+function ajustarDatosJupiter(){
+    gravedad=24.79;
+    document.getElementById("titulo").innerText+="Jupiter";
+    ajustarDatos();
+}
+function ajustarDatosSaturno(){
+    gravedad=10.44;
+    document.getElementById("titulo").innerText+="Saturno";
+    ajustarDatos();
+}
+function ajustarDatosUrano(){
+    gravedad=8.87;
+    document.getElementById("titulo").innerText+="Saturno";
+    ajustarDatos();
+}
+function ajustarDatosNeptuno(){
+    gravedad=11.15;
+    document.getElementById("titulo").innerText+="Saturno";
+    ajustarDatos();
+}
+function ajustarDatosLuna(){
+    gravedad=1.62;
+    document.getElementById("titulo").innerText+="Luna";
+    ajustarDatos();
+}
 function ajustarDatos(){
    document.querySelector(".modal-body").style.marginBottom="500px";
-   altura=2;
-   gravedad=9,8
+   console.log(altura);
+   pixelMetros=altura*100;
    tiempo=Math.sqrt((2*altura)/gravedad);
    velocidad=gravedad*tiempo;
-   
-   document.getElementById("titulo").innerText+="la tierra"
    if(bandera){
     document.querySelector(".modal-body").style.marginBottom="0px";
-   }
-   
+   }   
 }
+
 function reiniciar(){
+    document.querySelector(".modal-body").style.marginBottom="500px";
     contador=0;
     document.querySelector("#objeto").style.marginTop=contador+"px";
-    document.querySelector(".modal-body").style.marginBottom="500px";
     
 }
